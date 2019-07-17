50890bfb5e976fd6d33fd9658c42b43eb3d9fa31
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-17 14:46:03 +0200

    Change for new branch

--- ppt-graphql-server/index.js
+++ ppt-graphql-server/index.js
@@ -6,6 +6,7 @@ import typeDefs from "./schema";
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
+
 // This `listen` method launches a web-server.  
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
